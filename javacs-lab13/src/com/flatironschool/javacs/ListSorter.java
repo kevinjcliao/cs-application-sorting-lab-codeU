2b285d8f629e36cf1a5e71da80e5ea959770cc51
Kevin Liao <kevinliao@Atlantia.local>
2016-08-05 12:21:36 -0700

    Working.

--- javacs-lab13/src/com/flatironschool/javacs/ListSorter.java
+++ javacs-lab13/src/com/flatironschool/javacs/ListSorter.java
@@ -64,8 +64,55 @@ public class ListSorter<T> {
 	 */
 	public List<T> mergeSort(List<T> list, Comparator<T> comparator) {
         // FILL THIS IN!
-        return null;
+      System.out.println("list: " + list);
+      List<T> half1 = list.subList(0, list.size()/2);
+      List<T> half2 = list.subList(list.size()/2, list.size());
+      System.out.println("Half1: " + half1);
+      System.out.println("Half2: " + half2);
+      List<T> merged = this.merge(half1, half2, comparator);
+      System.out.println("merged: " + merged);
+      return this.msHelper(list, comparator);
 	}
+
+    private List<T> msHelper(List<T> list, Comparator<T> comparator) {
+        if(list.size() == 1) {
+            return list;
+        } else {
+            return merge(msHelper(list.subList(0, list.size()/2),
+                                  comparator),
+                         msHelper(list.subList(list.size()/2, list.size()),
+                                  comparator),
+                         comparator);
+        }
+    }
+
+    public List<T> merge(List<T> list1, List<T> list2, Comparator<T> comparator) {
+        System.out.println("Merge called for: " + list1 + " and " + list2);
+        return mergeHelper(list1, list2, new ArrayList<T>(), comparator);
+    }
+
+    private List<T> mergeHelper(List<T> list1, List<T> list2, List<T> currentList, Comparator<T> comparator) {
+        if(list1.size() == 0) {
+            System.out.println("Hit base case 1. List 1: " + list1 + " list 2: " + list2);
+            currentList.addAll(list2);
+            System.out.println("Returning list: " + currentList);
+            return currentList;
+
+        } else if (list2.size() == 0) {
+            System.out.println("Hit base case 2. List 1: " + list1 + " list 2: " + list2);
+            currentList.addAll(list1);
+            System.out.println("Returning list: " + currentList);
+            return currentList;
+        }
+        int flag = comparator.compare(list1.get(0), list2.get(0));
+        if (flag <= 0) {
+            currentList.add(list1.get(0));
+            return mergeHelper(list1.subList(1, list1.size()), list2, currentList, comparator);
+        } else {
+            currentList.add(list2.get(0));
+            return mergeHelper(list1, list2.subList(1, list2.size()), currentList, comparator);
+        }
+    }
 
 	/**
 	 * Sorts a list using a Comparator object.
@@ -75,7 +122,13 @@ public class ListSorter<T> {
 	 * @return
 	 */
 	public void heapSort(List<T> list, Comparator<T> comparator) {
-        // FILL THIS IN!
+      PriorityQueue<T> queue = new PriorityQueue<T>(list.size(), comparator);
+      queue.addAll(list);
+      list.clear();
+      while(!queue.isEmpty()) {
+          list.add(queue.poll());
+      }
+      return;
 	}
 
 	
@@ -89,8 +142,21 @@ public class ListSorter<T> {
 	 * @return
 	 */
 	public List<T> topK(int k, List<T> list, Comparator<T> comparator) {
-        // FILL THIS IN!
-        return null;
+      PriorityQueue<T> queue = new PriorityQueue<T>(k, comparator);
+      while(!list.isEmpty()) {
+          if(queue.size() < k) {
+              queue.offer(list.remove(0));
+          } else if(comparator.compare(list.get(0), queue.peek()) > 0) {
+              queue.poll();
+              queue.offer(list.remove(0));
+          } else {
+              list.remove(0);
+          }
+      }
+      while(!queue.isEmpty()) {
+          list.add(queue.poll());
+      }
+        return list;
 	}
 
 	
